38bae38650d77d5a493f9a6e8847c67a1df1a82e
gremdavel <MGD1981@gmail.com>
2013-07-15 17:04:00 -0400

    Revised test, fixed country_list (test found a border error!).

--- risk_test.py
+++ risk_test.py
@@ -6,10 +6,16 @@ def print_state(testboard):
 
 def test_counts(testboard):
     n = 0
-    for country in testboard.countries:
-        n += len(testboard.countries[country])
+    for continent in testboard.countries:
+        n += len(testboard.countries[continent])
     assert n == 42
+
+def test_borders(testboard):
+    for country in testboard.bordering_countries:
+        for bordering_country in testboard.bordering_countries[country]:
+            assert country in testboard.bordering_countries[bordering_country]
 
 
 testboard = risk.Board()
 test_counts(testboard)
+test_borders(testboard)
