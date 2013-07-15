2743a62ffbcf839163d193abdd9a0afff448ada7
gremdavel <MGD1981@gmail.com>
2013-07-15 16:47:34 -0400

    Working countries/card population functions/files.

--- risk.py
+++ risk.py
@@ -2,19 +2,28 @@
 class Board:
 
     def __init__(self):
-        self.country_list = open('risk_countries.txt')
-        self.continents = self.populate_continents()
+        self.country_lists = open('country_list.txt')
+        self.countries = self.populate_countries()
+        self.bordering_countries = self.populate_bordering_countries()
+        self.card_values = self.populate_card_values()
 
-    def populate_continents(self):
-        continents = {}
-        for line in self.country_list:
-            if 'CONTINENT' in line:
-                i = line.find(' ')
-                current_continent = line[i+1:-1]
-                continents[current_continent] = []
-            else:
-                continents[current_continent].append(line[:-1])
-        return continents
+    def populate_countries(self):
+        for line in self.country_lists:
+            if line[:9] == 'countries':
+                countries = eval(line[12:])
+                return countries
+
+    def populate_bordering_countries(self):
+        for line in self.country_lists:
+            if line[:19] == 'bordering_countries':
+                bordering_countries = eval(line[22:])
+                return bordering_countries
+
+    def populate_card_values(self):
+        for line in self.country_lists:
+            if line[:11] == 'card_values':
+                card_values = eval(line[14:])
+                return card_values
 
 
 if __name__ == '__main__':
